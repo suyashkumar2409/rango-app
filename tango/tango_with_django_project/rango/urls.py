f6c79366de15c7e6c33408c344c37edcdc711599
Suyash <f2014053@pilani.bits-pilani.ac.in>
2016-02-21 17:36:09 +0530

    Advanced Templating Learnt

--- tango/tango_with_django_project/rango/urls.py
+++ tango/tango_with_django_project/rango/urls.py
@@ -7,4 +7,9 @@ urlpatterns = patterns('',
 	url(r'^category/(?P<category_name_slug>[\w\-]+)/add_page/$',views.add_page,name='add_page'),
 	url(r'^category/(?P<category_name_slug>[\w\-]+)/$',views.category,name='category'),
 	url(r'^add_category/$',views.add_category,name='add_category'),
+	url(r'^$',views.index,name='restricted'),
+	url(r'^$',views.index,name='logout'),
+	url(r'^$',views.index,name='register'),
+	url(r'^$',views.index,name='login'),
+
 	)
